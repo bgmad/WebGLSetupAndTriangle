be1327d6bc978e8d728412c3a6ae24eba7315264
Ian Luchini <iancarloluchini@gmail.com>
2022-03-06 09:59:41 -0500

    Changed triangle to not be a solid color # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is ahead of 'origin/main' by 2 commits. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   app.js # # Untracked files: #	shaders.js # Both the vertex and fragment shaders needed to be modified. Since a new color attribute was added to the shaders, we need to get that separately. Now that we have the new color attribute location, we sep up another vertex attribute pointer. The layout is going to depend on how the data is set up. This new attribute also needs to be enabled.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const vertexShaderText = `
 precision mediump float;
 
 attribute vec2 vertPosition;
+attribute vec3 vertColor;
+varying vec3 fragColor;
 
 void main()
 {
+    fragColor = vertColor;
     gl_Position = vec4(vertPosition, 0.0, 1.0);
 }
 `;
@@ -12,9 +15,10 @@ void main()
 const fragmentShaderText = `
 precision mediump float;
 
+varying vec3 fragColor;
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(fragColor, 1.0);
 }
 `;
 
@@ -80,10 +84,10 @@ const InitDemo = () => {
     // Create buffer
     //
     const triangleVertices = 
-    [ // X, Y
-        0.0, 0.5,
-        -0.5, -0.5,
-        0.5, -0.5
+    [ // X, Y       R, G, B
+        0.0, 0.5,   1.0, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, -0.5,  0.0, 0.0, 1.0
     ];
 
     // Create the buffer object
@@ -94,16 +98,26 @@ const InitDemo = () => {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
 
     const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
     gl.vertexAttribPointer(
         positionAttribLocation, // Attribute location
         2, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
         0 // Offset from the beginning of a single vertex to this attribute
+    );
+    gl.vertexAttribPointer(
+        colorAttribLocation, // Attribute location
+        3, // Number of elements per attribute
+        gl.FLOAT, // Type of elements
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
     gl.enableVertexAttribArray(positionAttribLocation);
+    gl.enableVertexAttribArray(colorAttribLocation);
 
     //
     // Main render loop
